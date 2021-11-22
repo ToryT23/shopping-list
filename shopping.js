da4a99a56a7b01fe506d95c8c24c87f0a455d576
ToryT23 <torjtheu@gmail.com>
2021-11-22 15:49:18 -0600

    push to github

---
+++ shopping.js
@@ -0,0 +1,38 @@
+const groceryList = [
+{ 
+id: 1,
+name: "Milk",
+price: 4,
+},
+{
+id: 2,
+name: "Eggs",
+price: 3,
+},
+{
+id: 3,
+name: "Pizza Rolls",
+price: 7,
+},
+{
+id: 4,
+name: "Bologna",
+price: 4,
+},
+{
+id: 5,
+name: 'Apples',
+price: 3,
+},
+{
+id: 6,
+name: "Wheat Bread",
+price: 2,
+},
+{
+id: 7,
+name: "BBQ Sauce",
+price: 5,
+}
+]
+console.log(groceryList)
